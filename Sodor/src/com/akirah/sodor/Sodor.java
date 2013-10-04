91b0c42b43aedb92d9d8a248a024ea897386638a
Akip <akip.maulana@gmail.com>
2013-10-04 20:02:22 +0700

    Initial
    Pertama Push

---
+++ Sodor/src/com/akirah/sodor/Sodor.java
@@ -0,0 +1,60 @@
+package com.akirah.sodor;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.sodor.objek.Orang;
+
+public class Sodor implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	
+	Orang akip;
+	
+	@Override
+	public void create() {		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(1, h/w);
+		camera.setToOrtho(false, 800, 480);
+		batch = new SpriteBatch();
+		akip = new Orang();
+		
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		akip.dispose();
+	}
+
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+			akip.render();
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
